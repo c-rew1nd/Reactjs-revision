7fd9ef3cc5a2cf5d6c68f7079066fde6f86e8cd5
c-rew1nd <dreektseayger@gmail.com>
2024-08-12 19:25:20 +0530

    upated app.js and customreact.js

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,13 +1,15 @@
 import Chai from "./Chai"
 // refresh by using reload windows
 function App() {
-
+  const username='chai aur code'
   return (
     <> 
     <Chai/>
-    <h1>Chai aur react</h1>
-    </>
+    <h1>Chai aur react {username}</h1> 
+    <p>Test Para</p>
+    </>// {username} this is called as evaluated expression i.e, only the final outcome is written.
   )
 }
 // we use <> </> to pass the code as a single object
 export default App
+
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -2,9 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+    <div>
+        <h1>Custom App !</h1>
+    </div>
+    );
+}
+/*const ReactElemnt = {
+    type: 'a',
+    props:{
+        href:'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit Google' // the reason why ths code won't work here is because in custom react we were writting seperate custom render code to render it but here we are not writting any sort of such code
+}*/
+
+const anotherElement = (
+    <a href="http://google.com" target="_black">Visit Google</a>
+)
+const anotherUser = 'Dreekt'
+const ReactElemnt = React.createElement(
+        'a',
+        {
+            href: 'https://google.com',
+            target:"_blank"},
+            'Click me to visit google',
+            anotherUser // cannot write a proper function due to proper syntax.
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    //ReactElemnt
+   // <MyApp /> //MyApp()
+    anotherElement
 )
