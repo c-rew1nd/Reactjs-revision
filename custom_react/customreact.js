3556717d7f4b1df0758231113c1f1943e3831720
c-rew1nd <dreektseayger@gmail.com>
2024-08-12 17:54:08 +0530

    updated custom react

--- custom_react/customreact.js
+++ custom_react/customreact.js
@@ -1,10 +1,21 @@
-function customRender(reactElemnt,mainContainer){
+function customRender(reactElement,mainContainer){
+
+    /*
     const domElement = document.createElement
     (reactElemnt.type)
     domElement.innerHTML = reactElemnt.children
     domElement.setAttribute('href', reactElemnt.props.href )
     domElement.setAttribute('target', reactElemnt.props.target)
     constainer.appendChild(domElement)
+    */
+
+    const domElement = document.createElement (reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for(const prop in reactElement.props){
+        if(prop == 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
 }
 
 const reactElemnt = {
