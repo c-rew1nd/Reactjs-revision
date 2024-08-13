8651154fce4a5ceb6981c55d6a955daed3d14857
c-rew1nd <dreektseayger@gmail.com>
2024-08-13 13:18:43 +0530

    basic react counter project

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,22 +5,43 @@ import './App.css'
 
 function App() {
 
-  let counter = 5
+  let [counter, setCounter]=useState(0)
+
+  //let counter = 5
   const addValue = () => {
     console.log("added value", Date().toString());
-    counter = counter + 1
-    
+    //counter = counter + 1 
+    if(counter<20){ 
+      setCounter(counter + 1)
+    }
+    else{
+      console.log("value has to remian less than 21");
+    }
+    console.log(counter); // counter will be updated but no changes will appear on the UI
+  }
+  const removeValue = () =>{
+    console.log("decreased Value" , Date().toString());
+    if(counter>0){
+      setCounter(counter -1)
+    }
+    else{
+      console.log("value has to remian more than -1");
+    }
+      
+    console.log(counter)// this is updated later because it is comming from the set counter which is comming for the queue again.
   }
 
   return (
     <>
      <h1>Chai aur React</h1>
-     <h2>Counter Value: 5</h2>
+     <h2>Counter Value: {counter}</h2>
      <button
      onClick={addValue}
      >add value</button>
      <br />
-     <button>remove value</button>
+     <button
+     onClick={removeValue}
+     >remove value</button>
     </>
   )
 }
