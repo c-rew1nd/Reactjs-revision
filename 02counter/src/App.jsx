41b1920f116adbf5fdde49bfe83e0dafa65a08dc
c-rew1nd <dreektseayger@gmail.com>
2024-08-14 13:57:47 +0530

    updated

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -17,6 +17,19 @@ function App() {
     }
     console.log(counter); // counter will be updated but no changes will appear on the UI
   }
+
+  /*
+  const addValue =() =>{
+    setCounter(counter +1)
+    serCounter(counter +1) -> this will return 16 because this is sent a bunch of packages or batch task in fibre and since all are similar functions, set counter returns call back.
+  }
+  ---------------------------------------------------
+  const addValue =() =>{
+    setCounter(prevcounter => prevcounter +1)
+    serCounter(prevcounter => prevcounter +1) -> this will return 17  
+  }
+  */
+
   const removeValue = () =>{
     console.log("decreased Value" , Date().toString());
     if(counter>0){
