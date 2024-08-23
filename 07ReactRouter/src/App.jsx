74b619ccf8237c1273a2cbba5465651c821b130e
c-rew1nd <dreektseayger@gmail.com>
2024-08-23 10:58:51 +0530

    React Router Crash Course

---
+++ 07ReactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-gray-700'>React Dom</h1>
+    </>
+  )
+}
+
+export default App
