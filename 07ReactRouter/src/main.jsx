380b8356d4793682efa6835b0b4fff6ebeb5e3b0
c-rew1nd <dreektseayger@gmail.com>
2024-08-26 19:25:01 +0530

    created a default layou and used another way to route

--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -2,30 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import { Home, About, ContactUs } from './Components'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout/>,
-    children: [
-      {
-        path: '',
-        element: <Home/>
-      },
-      {
-        path:'about',
-        element: <About/>
-      },
-      {
-        path:'contact us',
-        element: <ContactUs/>
-      }
-    ]
-  }
-])
+//----------first way 
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home/>
+//       },
+//       {
+//         path:'about',
+//         element: <About/>
+//       },
+//       {
+//         path:'contact us',
+//         element: <ContactUs/>
+//       }
+//     ]
+//   }
+// ])
+
+//---------second way
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact us' element={<ContactUs/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
