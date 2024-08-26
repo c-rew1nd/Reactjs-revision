d8d87ef5702bfd9ecbd917754225923327a64606
c-rew1nd <dreektseayger@gmail.com>
2024-08-26 19:14:38 +0530

    created home page and added about, contact us

--- 07ReactRouter/src/Components/Index.js
+++ 07ReactRouter/src/Components/Index.js
@@ -1,5 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Home from "./Home/Home";
+import About from "./About/About"
+import ContactUs from "./ContactUs/ContactUs"
 
-export {Header, Home, Footer}
+export {Header, Home, About, ContactUs, Footer}
--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import { Home, About, ContactUs } from './Components'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: '',
+        element: <Home/>
+      },
+      {
+        path:'about',
+        element: <About/>
+      },
+      {
+        path:'contact us',
+        element: <ContactUs/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
