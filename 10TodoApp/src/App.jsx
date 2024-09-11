c97102bf9809aa4af42896215aecfafe88f94b13
c-rew1nd <dreektseayger@gmail.com>
2024-09-11 11:51:51 +0530

    created updateTodo

--- 10TodoApp/src/App.jsx
+++ 10TodoApp/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Todoprovider } from './Context'
+import { Todoprovider } from './contexts'
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -11,8 +11,12 @@ function App() {
       setTodos((prev) => [{id: Date.now(), ...todo},...prev])
   }
 
+  const updateTodo = (id,todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
   return (
-    <Todoprovider value={{todos, addTodo,updateTodo,deleteTodo,toggleComplete}} >
+    <Todoprovider value={{todos, addTodo,updateTodo, deleteTodo, toggleComplete}} >
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
