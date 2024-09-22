006606c365ca3c2a87e1e2ba02a6a48397260676
c-rew1nd <dreektseayger@gmail.com>
2024-09-22 16:31:00 +0530

    updated todoReducer

--- reduxtoolkittodo/src/app/store.js
+++ reduxtoolkittodo/src/app/store.js
@@ -1,3 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice'
 
-export const store = configureStore({});
+/**
+ * Configures the Redux store with the specified reducer.
+ *
+ * @type {Object}
+ * @property {Function} reducer - The root reducer function for the Redux store.
+ */
+export const store = configureStore({
+    reducer: {
+        // The key here should match the name of the slice in your createSlice call
+        todo: todoReducer
+    }
+});
