8632cba87195a5e9dfbc6986db809d92bda54af2
c-rew1nd <dreektseayger@gmail.com>
2024-09-22 20:49:16 +0530

    completed react redux project

--- reduxtoolkittodo/src/features/todo/todoSlice.js
+++ reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -1,71 +1,28 @@
-import {createSlice, nanoid} from '@reduxjs/toolkit';
+import {createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [
-        {
-            id:1,
-            text: "Hello-world"
-        }
-    ]
+    todos: [{id: 1, text: "Hello world"}]
 }
 
-export const todoSlice = new createSlice({
+
+
+export const todoSlice = createSlice({
     name: 'todo',
     initialState,
-    reducers :{
-/*
- * Adds a new todo item to the state.
- *
- * {Object} state - The current state of the todos.
- * {Object} action - The action dispatched.
- * {Object} action.payload - The payload of the action.
- * {string} action.payload.text - The text of the new todo item.
- */
+    reducers: {
         addTodo: (state, action) => {
             const todo = {
-                id: nanoid(),
-                text: action.payload.text
+                id: nanoid(), 
+                text: action.payload
             }
             state.todos.push(todo)
         },
-        /**
- * Removes a todo item from the state based on the provided action payload.
- *
- *  {Object} state - The current state of the todos.
- *  {Object} action - The action object containing the payload.
- *  {number|string} action.payload - The ID of the todo item to be removed.
- */
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) =>
-                todo.id !== action.payload)
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
         },
-/**
- * Deletes a todo item from the state based on the provided action payload.
- *
- * {Object} state - The current state of the todos.
- * {Object} action - The action object containing the payload.
- * {number|string} action.payload - The ID of the todo item to be deleted.
- */
-        deleteTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
-        },
-/**
- * Updates a todo item in the state based on the provided action payload.
- *
- * {Object} state - The current state of the todos.
- * {Object} action - The action object containing the payload.
- * {Object} action.payload - The payload containing the ID of the todo item to be updated and the new data.
- * {number|string} action.payload.id - The ID of the todo item to be updated.
- * {Object} action.payload.data - The new data for the todo item.
- */
-        updateTodo:(state, action) => {
-            state.todos = state.todos.map((todo) => 
-                todo.id === action.payload.id ? { ...todo, ...action.payload.text } : todo
-            )
-        }
     }
 })
 
-export const {addTodo, removeTodo, deleteTodo, updateTodo} = todoSlice.actions
+export const {addTodo, removeTodo} = todoSlice.actions
 
 export default todoSlice.reducer
